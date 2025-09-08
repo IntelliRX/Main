dc0bfd934c8bcb4ac5b104c1adb8b6616026fd1f
intellirx <intellirxapp@gmail.com>
2025-09-08 22:49:47 +0530

    login related changes
    moved logout button to home.component.html
    used google existing button got login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { User } from 'firebase/auth';
+import { LoginService } from "../common/services/login.service";
+import { Router } from '@angular/router';  
 
 @Component({
     selector: 'app-home',
@@ -6,5 +9,15 @@ import { Component } from "@angular/core";
     styleUrl: './home.component.css'
 })
 export class HomeComponent {
-    
+   user: User | null = null;
+    constructor(private loginService: LoginService, private router: Router) {
+        this.user = this.loginService.getUser();
+    }
+
+    async signOut() {
+        alert("Logging out");
+        await this.loginService.logout();
+        this.user = null;
+        this.router.navigate(["/login"]);
+  }
 }
