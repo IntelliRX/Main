aead8fc0cd0626d1f90d54b503d839837d1d9749
Nakul Kundra <intellirxapp@gmail.com>
2025-09-16 22:48:52 +0530

    Revert "Fix SSR prerendering error"
    This reverts commit 7448ff875987fba648d034e3ec92216c4c1304fd.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,19 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '/login',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: '/home',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: '/patients',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: '/search',
+    path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
