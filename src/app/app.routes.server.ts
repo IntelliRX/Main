7448ff875987fba648d034e3ec92216c4c1304fd
IntelliRX <intellirxapp@gmail.com>
2025-09-16 22:27:37 +0530

    Fix SSR prerendering error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '/login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/patients',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/search',
     renderMode: RenderMode.Prerender
   }
 ];
